76b82b1a85e2f7cbac2d9d6c91f9f67b71b3916b
Kishore G <153879617+Kishore2322@users.noreply.github.com>
2025-08-02 18:38:22 +0000

    typeconversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,23 +1,23 @@
-// let score = "33abc"
+let score = "33abc"
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
 
-// console.log(valueInNumber);
-
+console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN(value)  but type is still number (valueInNumber)
 // true => 1; false => 0
 
-// let isLoggedIn = "Kishore"
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(typeof booleanIsLoggedIn);
-// console.log(booleanIsLoggedIn);
+let isLoggedIn = "Kishore"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -28,4 +28,38 @@
  let stringNumber = String(someNumber)
  console.log(typeof stringNumber);
  console.log(someNumber);
- 
+
+// ********************** operations ****************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  power
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = "  Kishore"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2" + 2);
+
+console.log(+true);
+console.log(+"");
+
+//not recommended
+let num1, num2, num3
+num1 = num2 = num3  = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
