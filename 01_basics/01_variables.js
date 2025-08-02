45651a38f6e0fa2d1213c951fa04eb5d1d54ca29
Kishore G <153879617+Kishore2322@users.noreply.github.com>
2025-08-02 17:21:00 +0000

    dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,5 +2,18 @@ const accountId = 14423
 let accountEmail = "Kishoreg4200@gmail.com"
 var accountPassword = 345
 accountCity = "Bengaluru"
+let accountState;
+
+// accountId = 2   //not allowed to change const variable
+
+accountEmail = "Kishoreg14881@gmail.com"
+accountPassword = 34512
+accountCity = "Bengaluru"
 
 console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3 + 3) //we are using node.js, not browser
+
+console.log(3 + 3);
+
+console.log("Kishore")
+
+
+let name = "Kishore"
+let age = 18
+let isLoggedin = false
+
+//primitive datatypes
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value   it's datatype is object
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);     //undefined
+console.log(typeof null);   //object
